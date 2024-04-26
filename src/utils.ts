fad6f168b02b4c369c0b5d77dc3f05ea5ff3984c
Pwojcik2 <70239167+Pwojcik2@users.noreply.github.com>
2024-04-26 14:20:13 -0700

    util file stup

---
+++ src/utils.ts
@@ -0,0 +1,8 @@
+import { KaboomCtx } from "kaboom";
+import { scale } from "./constants";
+
+export async function makeMap(k: KaboomCtx, name: string) {
+    const mapData = await (await fetch(`./${name}.json`)).json();
+
+    const map = k.make()
+}
